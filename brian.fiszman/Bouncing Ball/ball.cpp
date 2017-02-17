388069d0a39074172ac233c28f457f5240c4ef26
Brian Fiszman <brianezequielfiszman@gmail.com>
2017-02-17 01:41:32 -0300

    Added a shortening constant

--- brian.fiszman/Bouncing Ball/ball.cpp
+++ brian.fiszman/Bouncing Ball/ball.cpp
@@ -2,30 +2,32 @@
 
 using namespace sf;
 
-#define NORMAL_SPEED    0.1f
+#define IDLE_SPEED      0.1f
 #define MOVEMENT_SPEED  2.f
-#define ACCELERATION    NORMAL_SPEED/1
+#define ACCELERATION    IDLE_SPEED/1
 #define GRAVITY         ACCELERATION*9.81
 #define SLOWDOWN        0.005
 
-#define UP      Keyboard::Up
-#define DOWN    Keyboard::Down
-#define RIGHT   Keyboard::Right
-#define LEFT    Keyboard::Left
+#define BPOS            ball.getGlobalBounds()
+#define RPOS            rectangle.getGlobalBounds()
+
+#define UP              Keyboard::Up
+#define DOWN            Keyboard::Down
+#define RIGHT           Keyboard::Right
+#define LEFT            Keyboard::Left
 
 int main()
 {
-    int resolutionX = 800;
-    int resolutionY = 600;
+    int resX = 800;
+    int resY = 600;
     float speedX = 0, speedY = 0;
    
-    RenderWindow window(VideoMode(resolutionX, resolutionY), "Bouncing Ball!");
+    RenderWindow window(VideoMode(resX, resY), "Bouncing Ball!");
     window.setFramerateLimit(60);
-    CircleShape shape(10.f);
+    CircleShape ball(10.f);
     RectangleShape rectangle(Vector2f(120, 5)); 
-    shape.setFillColor(Color::Blue);
-
-    rectangle.setPosition(resolutionX/2, resolutionY/2);
+    rectangle.setPosition(resX/2, resY/2);
+    ball.setFillColor(Color::Blue);
     while (window.isOpen())
     {
         Event event;
@@ -44,17 +46,15 @@ int main()
         if(Keyboard::isKeyPressed(RIGHT))   speedX += MOVEMENT_SPEED; 
         if(Keyboard::isKeyPressed(LEFT))    speedX -= MOVEMENT_SPEED; 
         
-        speedX = ((shape.getGlobalBounds().left < 0 && speedX < 0) ||
-                (shape.getGlobalBounds().left+shape.getGlobalBounds().width > resolutionX && speedX > 0)) ?
+        speedX = ((BPOS.left < 0 && speedX < 0) || (BPOS.left+BPOS.width > resX && speedX > 0)) ?
             -speedX : speedX;
-        speedY = ((shape.getGlobalBounds().top < 0 && speedY < 0) ||
-                (shape.getGlobalBounds().top+shape.getGlobalBounds().height > resolutionY && speedY > 0)) ?
+        speedY = ((BPOS.top < 0 && speedY < 0) || (BPOS.top+BPOS.height > resY && speedY > 0)) ?
             -speedY : speedY;
         
-        shape.move(speedX*NORMAL_SPEED, speedY*NORMAL_SPEED);
+        ball.move(speedX*IDLE_SPEED, speedY*IDLE_SPEED);
 
         window.clear();
-        window.draw(shape);
+        window.draw(ball);
         window.draw(rectangle);
         window.display();
     }
