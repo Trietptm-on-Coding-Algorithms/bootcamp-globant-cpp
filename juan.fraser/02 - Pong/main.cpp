bbfed6719a7c4a56d510af0572c187255252ec85
johnfraser <john.fraser.s@gmail.com>
2017-02-17 12:28:17 -0300

    barra con controles y colisiones

--- juan.fraser/02 - Pong/main.cpp
+++ juan.fraser/02 - Pong/main.cpp
@@ -17,29 +17,45 @@ int main() {
 	window.setFramerateLimit(60);
 
 	sf::CircleShape ball(10.f);
-	//ball.setPointCount(8);
-	double vx=0, vy=0, ax=0, ay=9.8;
-	double dv=0.1, da=0.1;
-	double sd = 0.001;
+	float vx=20, vy=0, ax=0, ay=9.8f;
+	float dv=0.01f, da=0.01f;
+	float sd = 0.0001f;
 
+	sf::RectangleShape rectangle(sf::Vector2f(16.f,64.f) );
+	rectangle.setPosition(4, (screenY-rectangle.getGlobalBounds().height)/2);
+	float dm = 0.5f;
+
+	printf("WS to move rectangle. \nArrows to move ball.");
+
+	sf::Clock clock;
 	sf::Event event;
-	bool justLostFocus = true;
+	bool lostFocus = false;
 	while (window.isOpen())
 	{
 		//Window Management
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed)
 				window.close();
-		}
-		if (window.hasFocus())
-			justLostFocus = true;
-		else {
-			if (justLostFocus) {
+			else if (event.type == sf::Event::LostFocus) {
 				window.requestFocus();
-				justLostFocus = false;
+				lostFocus = true;
 			}
-		}
+			else if (event.type == sf::Event::GainedFocus) {
+				lostFocus = false;
+				clock.restart();
+			}
 				
+		}
+
+		//Unfocus "Pause"
+		if (lostFocus)
+			continue;
+		
+		//Time Elapsed
+		int dt = clock.restart().asMilliseconds();
+		if (dt > 100)
+			dt = 100;
+
 		//Handle Keyboard events
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
 			vx -= 1.f;
@@ -49,14 +65,29 @@ int main() {
 			vy -= 1.f;
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
 			vy += 1.f;
-		
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+			rectangle.move(0, -dm*dt);
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+			rectangle.move(0, dm*dt);
+
 		//Phisics Updates
-		vx += ax*da;
-		vy += ay*da;
-		vx -= sd*vx;
-		vy -= sd*vy;
+		vx += ax*da*dt;
+		vy += ay*da*dt;
+		vx -= sd*vx*dt;
+		vy -= sd*vy*dt;
 
 		//Collisions
+		if (rectangle.getGlobalBounds().top < 0)
+			rectangle.setPosition(4, 0);
+		if (rectangle.getGlobalBounds().top + rectangle.getGlobalBounds().height > screenY)
+			rectangle.setPosition(4, screenY - rectangle.getGlobalBounds().height);
+
+		if (ball.getGlobalBounds().left < rectangle.getGlobalBounds().left + rectangle.getGlobalBounds().width 
+			&& ball.getGlobalBounds().top < rectangle.getGlobalBounds().top + rectangle.getGlobalBounds().height
+			&& ball.getGlobalBounds().top + ball.getGlobalBounds().height > rectangle.getGlobalBounds().top)
+			vx = -vx;
+
 		if (ball.getGlobalBounds().left < 0 && vx < 0)
 			vx = -vx;
 		if (ball.getGlobalBounds().top < 0 && vy < 0)
@@ -69,11 +100,14 @@ int main() {
 			vy = -vy;
 		
 		//Object Update
-		ball.move((float)vx*dv, (float)vy*dv);
+		ball.move(vx*dv*dt, vy*dv*dt);
 		
 		//Renders
 		window.clear();
+
 		window.draw(ball);
+		window.draw(rectangle);
+
 		window.display();
 	}
 	return 0;
