1267535f69242f4754d9913647e8a34264e293dd
Brian Fiszman <brianezequielfiszman@gmail.com>
2017-02-17 01:28:44 -0300

    Corrected ternary opperation

--- brian.fiszman/Bouncing Ball/ball.cpp
+++ brian.fiszman/Bouncing Ball/ball.cpp
@@ -18,11 +18,14 @@ int main()
     int resolutionX = 800;
     int resolutionY = 600;
     float speedX = 0, speedY = 0;
-    
+   
     RenderWindow window(VideoMode(resolutionX, resolutionY), "Bouncing Ball!");
     window.setFramerateLimit(60);
     CircleShape shape(10.f);
+    RectangleShape rectangle(Vector2f(120, 5)); 
     shape.setFillColor(Color::Blue);
+
+    rectangle.setPosition(resolutionX/2, resolutionY/2);
     while (window.isOpen())
     {
         Event event;
@@ -35,6 +38,7 @@ int main()
         speedY += GRAVITY;
         speedY -= SLOWDOWN*speedY;
         speedX -= SLOWDOWN*speedX;
+
         if(Keyboard::isKeyPressed(UP))      speedY -= MOVEMENT_SPEED; 
         if(Keyboard::isKeyPressed(DOWN))    speedY += MOVEMENT_SPEED; 
         if(Keyboard::isKeyPressed(RIGHT))   speedX += MOVEMENT_SPEED; 
@@ -44,13 +48,14 @@ int main()
                 (shape.getGlobalBounds().left+shape.getGlobalBounds().width > resolutionX && speedX > 0)) ?
             -speedX : speedX;
         speedY = ((shape.getGlobalBounds().top < 0 && speedY < 0) ||
-                (shape.getGlobalBounds().top+shape.getGlobalBounds().width > resolutionY && speedY > 0)) ?
+                (shape.getGlobalBounds().top+shape.getGlobalBounds().height > resolutionY && speedY > 0)) ?
             -speedY : speedY;
         
         shape.move(speedX*NORMAL_SPEED, speedY*NORMAL_SPEED);
 
         window.clear();
         window.draw(shape);
+        window.draw(rectangle);
         window.display();
     }
 
