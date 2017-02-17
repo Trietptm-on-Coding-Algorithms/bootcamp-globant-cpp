505a850247cc0c64d96fb1c1ad2efe16a03aee60
Brian Fiszman <brianezequielfiszman@gmail.com>
2017-02-16 22:11:17 -0300

    Added some parameters

--- brian.fiszman/ball.cpp
+++ brian.fiszman/ball.cpp
@@ -5,6 +5,8 @@ using namespace sf;
 
 #define NORMAL_SPEED    0.1f
 #define FAST_SPEED      0.25f
+#define ACCELERATION    NORMAL_SPEED/0.2
+#define GRAVITY         NORMAL_SPEED*9.81
 
 #define UP      Keyboard::Up
 #define DOWN    Keyboard::Down
@@ -13,10 +15,14 @@ using namespace sf;
 
 int main()
 {
-    RenderWindow window(VideoMode(500, 500), "SFML works!");
+    int resolutionX = 800;
+    int resolutionY = 600;
+    float speedX = 0, speedY = 0;
+    
+    RenderWindow window(VideoMode(resolutionX, resolutionY), "Bouncing Ball!");
+    window.setFramerateLimit(60);
     CircleShape shape(10.f);
     shape.setFillColor(Color::Blue);
-
     while (window.isOpen())
     {
         Event event;
@@ -26,10 +32,22 @@ int main()
                 window.close();
         }
         
-        if(Keyboard::isKeyPressed(UP))      shape.move(0, -NORMAL_SPEED);
-        if(Keyboard::isKeyPressed(DOWN))    shape.move(0, NORMAL_SPEED);
-        if(Keyboard::isKeyPressed(RIGHT))   shape.move(NORMAL_SPEED, 0);
-        if(Keyboard::isKeyPressed(LEFT))    shape.move(-NORMAL_SPEED, 0);       
+
+        if(Keyboard::isKeyPressed(UP))      speedY -= NORMAL_SPEED;
+        if(Keyboard::isKeyPressed(DOWN))    speedY += NORMAL_SPEED;
+        if(Keyboard::isKeyPressed(RIGHT))   speedX += NORMAL_SPEED;
+        if(Keyboard::isKeyPressed(LEFT))    speedX -= NORMAL_SPEED;
+
+       
+        if((shape.getGlobalBounds().left < 0 && speedX < 0) ||
+                (shape.getGlobalBounds().left+shape.getGlobalBounds().width > resolutionX && speedX > 0))
+            speedX -= speedX;
+        if((shape.getGlobalBounds().top < 0 && speedY < 0) ||
+                (shape.getGlobalBounds().top+shape.getGlobalBounds().width > resolutionY && speedY > 0))
+            speedY -= speedY;
+        
+
+        shape.move(speedX, speedY);
 
         window.clear();
         window.draw(shape);
